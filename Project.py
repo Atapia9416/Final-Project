078db2af9658943c03ee6b08ae92c20bd7bba701
Atapia9416 <atapia9416@mail.fresnostate.edu>
2021-05-06 18:45:00 -0700

    finished emi

--- Project.py
+++ Project.py
@@ -29,16 +29,26 @@ def Emi():
     input1 = input("Enter amount of loan: ") 
     input2= input("Enter interest rate: ") 
     input3 = input("Enter number of years: ") 
-    A=float(input1)
-    r=float(input2)
-    n=float(input3)
+    A=float(input1) # Principle
+    r=float(input2) # rate
+    n=float(input3) # years
+    mr=float(r/12)  #monthly rate
+    cb = A #closing balance
     i=r/1200
     emi=(A*i)/(1-(1+i)**(-12*n))
-    print("Monthly Payment:",'$'+str('%.2f'%emi))
-    print ("Payment #".ljust(20), "Payment Amount".ljust(20), "Interest Payed".ljust(20), "Ending Balance".ljust(20)) 
+    print("Monthly Payment:",'$'+str('%.2f\n'%emi))
+    print ("Payment #".ljust(20), "Payment Amount".ljust(20), "Interest Payed".ljust(20), "Principle Payed".ljust(20), "Ending Balance\n".ljust(20)) 
     count = 0
+    print(str(cb).rjust(91))
+
 
     while (count < n*12):
         count += 1
-        print(str(count).ljust(20))
+        ip = ((mr/100)*cb) #Monthly interest payed
+        emi = emi
+        pp = emi - ip
+        cb = cb - pp
+        print(str(count).ljust(20), str('%.2f'%emi).ljust(20), str('%.2f'%ip).ljust(20), str('%.2f'%pp).ljust(20), str('%.2f'%cb).ljust(20)) 
+        
+
 Emi()
